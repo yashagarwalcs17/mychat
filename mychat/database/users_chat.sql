7278677e5950952b3aa154c3b5d694bcbb865ab1
yashagarwalcs17 <46733509+yashagarwalcs17@users.noreply.github.com>
2020-06-29 03:09:40 +0530

    Add files via upload

---
+++ mychat/database/users_chat.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 28, 2020 at 11:34 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mychat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_chat`
+--
+
+CREATE TABLE `users_chat` (
+  `msg_id` int(11) NOT NULL,
+  `sender_username` varchar(100) NOT NULL,
+  `receiver_username` varchar(100) NOT NULL,
+  `msg_content` varchar(255) NOT NULL,
+  `msg_status` text NOT NULL,
+  `msg_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users_chat`
+--
+ALTER TABLE `users_chat`
+  ADD PRIMARY KEY (`msg_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users_chat`
+--
+ALTER TABLE `users_chat`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
